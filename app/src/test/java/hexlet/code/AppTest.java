a6ff1032f90f22b28be43886f100743c12c1ae2a
Aleksandra Volkova <91-ksandra@mail.ru>
2024-07-16 12:55:40 +0600

    Stage6

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,20 +1,25 @@
 package hexlet.code;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import hexlet.code.model.Url;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import hexlet.code.repository.UrlRepository;
 
 public class AppTest {
 
     Javalin app;
 
     @BeforeEach
-    public final void setUp() throws IOException, SQLException {
+    public final void setUp() throws SQLException {
         app = App.getApp();
     }
 
@@ -26,4 +31,43 @@ public class AppTest {
             assertThat(response.body().string()).contains("Welcome to Site Analyzer");
         });
     }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=https://www.google.com";
+            var response = client.post("/urls", requestBody);
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("https://www.google.com");
+        });
+
+
+    }
+
+    @Test
+    public void testUrlPage() throws SQLException {
+        var url = new Url("https://www.google.com", Timestamp.valueOf(LocalDateTime.now()));
+        UrlRepository.save(url);
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/" + url.getId());
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    void testUrlNotFound() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/999999");
+            assertThat(response.code()).isEqualTo(404);
+        });
+    }
+
 }
